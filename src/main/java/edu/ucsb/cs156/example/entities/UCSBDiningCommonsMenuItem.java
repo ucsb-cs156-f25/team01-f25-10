497550388a3bbf08b39f90f1dcbf3a46beebd2ed
Sarah Yu <sarahyu@ucsb.edu>
2025-10-27 13:45:26 -0700

    added UCSBDiningCommonsMenuItem controller and controller tests, fixed casing issue in entity

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -19,7 +19,7 @@ public class UCSBDiningCommonsMenuItem {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  private String diningCommonsCode;
+  private String diningcommonscode;
   private String name;
   private String station;
 }
