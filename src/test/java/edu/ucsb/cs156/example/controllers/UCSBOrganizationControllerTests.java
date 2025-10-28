d62623ee368b873aa8e75d206309f947d8eef686
Saanvi Pabbichetty <spabbichetty@ucsb.edu>
2025-10-28 16:17:26 -0700

    added UCSBOrganizationController and testing methods

---
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationControllerTests.java
@@ -0,0 +1,138 @@
+package edu.ucsb.cs156.example.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import edu.ucsb.cs156.example.ControllerTestCase;
+import edu.ucsb.cs156.example.entities.UCSBOrganization;
+import edu.ucsb.cs156.example.repositories.UCSBOrganizationRepository;
+import edu.ucsb.cs156.example.repositories.UserRepository;
+import edu.ucsb.cs156.example.testconfig.TestConfig;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+@WebMvcTest(controllers = UCSBOrganizationController.class)
+@Import(TestConfig.class)
+public class UCSBOrganizationControllerTests extends ControllerTestCase {
+
+  @MockBean UCSBOrganizationRepository ucsbOrganizationRepository;
+
+  @MockBean UserRepository userRepository;
+
+  // Authorization tests for /api/ucsborganization/admin/all
+
+  @Test
+  public void logged_out_users_cannot_get_all() throws Exception {
+    mockMvc
+        .perform(get("/api/ucsborganization/all"))
+        .andExpect(status().is(403)); // logged out users can't get all
+  }
+
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void logged_in_users_can_get_all() throws Exception {
+    mockMvc.perform(get("/api/ucsborganization/all")).andExpect(status().is(200)); // logged
+  }
+
+  // Authorization tests for /api/ucsborganization/post
+  // (Perhaps should also have these for put and delete)
+
+  @Test
+  public void logged_out_users_cannot_post() throws Exception {
+    mockMvc.perform(post("/api/ucsborganization/post")).andExpect(status().is(403));
+  }
+
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void logged_in_regular_users_cannot_post() throws Exception {
+    mockMvc
+        .perform(post("/api/ucsborganization/post"))
+        .andExpect(status().is(403)); // only admins can post
+  }
+
+  // Tests with mocks for database actions
+
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void logged_in_user_can_get_all_ucsborganizations() throws Exception {
+
+    // arrange
+    UCSBOrganization org1 =
+        UCSBOrganization.builder()
+            .orgcode("ACM")
+            .orgTranslationShort("ACM")
+            .orgTranslation("Association for Computing Machinery")
+            .inactive(false)
+            .build();
+
+    UCSBOrganization org2 =
+        UCSBOrganization.builder()
+            .orgcode("SWE")
+            .orgTranslationShort("SWE")
+            .orgTranslation("Society of Women Engineers")
+            .inactive(false)
+            .build();
+    ArrayList<UCSBOrganization> expectedOrgs = new ArrayList<>(Arrays.asList(org1, org2));
+
+    when(ucsbOrganizationRepository.findAll()).thenReturn(expectedOrgs);
+
+    // act
+    MvcResult response =
+        mockMvc.perform(get("/api/ucsborganization/all")).andExpect(status().isOk()).andReturn();
+
+    // assert
+    verify(ucsbOrganizationRepository, times(1)).findAll();
+    String expectedJson = mapper.writeValueAsString(expectedOrgs);
+    String responseString = response.getResponse().getContentAsString();
+    assertEquals(expectedJson, responseString);
+  }
+
+  @WithMockUser(roles = {"ADMIN", "USER"})
+  @Test
+  public void an_admin_user_can_post_a_new_organization() throws Exception {
+    // arrange
+
+    UCSBOrganization org =
+        UCSBOrganization.builder()
+            .orgcode("ACM")
+            .orgTranslationShort("ACM")
+            .orgTranslation("Association for Computing Machinery")
+            .inactive(true)
+            .build();
+
+    when(ucsbOrganizationRepository.save(eq(org))).thenReturn(org);
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                post("/api/ucsborganization/post?orgcode=ACM&orgTranslationShort=ACM&orgTranslation=Association for Computing Machinery&inactive=true")
+                    .with(csrf()))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // assert
+    verify(ucsbOrganizationRepository, times(1)).save(eq(org));
+    String expectedJson = mapper.writeValueAsString(org);
+    String responseString = response.getResponse().getContentAsString();
+    assertEquals(expectedJson, responseString);
+    UCSBOrganization responseOrg = mapper.readValue(responseString, UCSBOrganization.class);
+    assertEquals("ACM", responseOrg.getOrgcode());
+    assertEquals("ACM", responseOrg.getOrgTranslationShort());
+    assertEquals("Association for Computing Machinery", responseOrg.getOrgTranslation());
+    assertEquals(true, responseOrg.getInactive());
+  }
+}
