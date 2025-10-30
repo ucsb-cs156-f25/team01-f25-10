c22a2223f4223ad149cbd53a22165ff1a23058ca
Charlie Nava <charlie220@ucsb.edu>
2025-10-29 20:23:14 -0700

    added import for column

--- src/main/java/edu/ucsb/cs156/example/entities/Articles.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -1,5 +1,6 @@
 package edu.ucsb.cs156.example.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
