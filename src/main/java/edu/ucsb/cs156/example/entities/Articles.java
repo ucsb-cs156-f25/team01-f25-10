279c95d9075c255852aabbf9800dafbc2c4286e5
Charlie Nava <charlie220@ucsb.edu>
2025-10-29 19:30:02 -0700

    added riht import

--- src/main/java/edu/ucsb/cs156/example/entities/Articles.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -4,10 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.LocalDateTime;
 import lombok.NoArgsConstructor;
 
 @Data
